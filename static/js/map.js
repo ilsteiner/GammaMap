beca1f3ee16805b59513d5f866d7e6a46015fd7f
ilsteiner <izakizak@gmail.com>
2016-02-10 17:15:56 -0500

    I think I got it working, need remote KML to test.

--- static/js/map.js
+++ static/js/map.js
@@ -1,19 +1,22 @@
-var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+function initMap() {
+    var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"})
 
-var mapOptions = {
-    zoom: 11,
-    center: new google.maps.LatLng(55.6468, 37.581),
-    mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-    }
-};
+    var mapOptions = {
+        zoom: 11,
+        center: {lat: 40.7127, lng: -74.0059},
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+        }
+    };
 
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
-    map: map
-});
+    var ctaLayer = new google.maps.KmlLayer({
+        //url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
+        url: 'https://drive.google.com/file/d/0B99r3d3UJK4vWDVfRi1EaXQ0U2M/view?usp=sharing',
+        map: map
+    });
 
-map.mapTypes.set('map_style', styledMap);
-map.setMapTypeId('map_style');
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+}
